45cb57de3d0fba0f19f8f52e983b759bf7b81c5c
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-11-14 14:48:55 -0400

    deploy

--- src/hooks/useProducto.ts
+++ src/hooks/useProducto.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Product, ProductFilters, ProductService } from '@/services/ProductService';
-
+ 
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
